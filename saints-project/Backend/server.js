6fc7ee710b102c2521ba47cc538ecc6ba3b29cc9
ShadowMaster <81726544+Marksedrak@users.noreply.github.com>
2026-04-03 14:21:40 -0400

    Added Database functionality, changes some styles mainly to index page, and changes server.js as to print (in console) the added users

--- saints-project/Backend/server.js
+++ saints-project/Backend/server.js
@@ -1,5 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const db = require("better-sqlite3")("database.db");
+db.pragma("journal_mode = WAL");
+
+// Database setup
+
+const createTables = db.transaction(() => {
+  db.prepare(
+    `
+    CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username STRING NOT NULL UNIQUE,
+    password STRING NOT NULL  
+    )`,
+  ).run();
+});
+
+createTables();
+
+// End Database setup
+
 const app = express();
 
 app.use(express.json());
@@ -10,9 +30,22 @@ app.post("/api/data", async (req, res) => {
   // Handle Login attempt
   const { username, password } = req.body;
   console.log(`${username}, ${password}`);
-  res
-    .status(200)
-    .json({ text: `Received username: ${username} and password: ${password}` });
+  // Validate username and password
+
+  // Send response back to frontend
+  db.prepare(`INSERT INTO users (username, password) VALUES (?, ?)`).run(
+    username,
+    password,
+  );
+
+  db.prepare(`SELECT * FROM users`)
+    .all()
+    .forEach((row) => {
+      console.log(row);
+    });
+  res.status(200).json({
+    text: `Username: ${username}\nPassword: ${password}`,
+  });
 });
 
 app.get("/api/data", async (req, res) => {
--- saints-project/app/index.tsx
+++ saints-project/app/index.tsx
@@ -1,5 +1,5 @@
 import { SimpleButton } from "@/components/SimpleButton";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   KeyboardAvoidingView,
   Platform,
@@ -9,27 +9,40 @@ import {
   View,
 } from "react-native";
 
+const DEV_IP = "10.15.46.95";
+
 export default function Index() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState<string | null>(null);
   const Pass = useRef<TextInput>(null);
   const disabled = !username || !password;
+
+  const ShowErrors = useEffect(() => {
+    if (errors) {
+      alert(errors);
+    }
+  }, [errors]);
 
   async function handleLogin() {
     if (disabled) {
       return;
     } else {
-      //message = `${username}, ${password}`;
       try {
-        const response = await fetch("http://localhost:3000/api/data", {
+        // TODO: Replace with actual endpoint URL when ready
+        const response = await fetch(`http://${DEV_IP}:3000/api/data`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ username, password }),
+          body: JSON.stringify({
+            username: username,
+            password: password,
+          }),
         });
 
         const message = await response.json();
+        setErrors(message.errors);
         alert(message.text);
       } catch (error) {
         alert("An error occurred while trying to log in.");
@@ -39,7 +52,11 @@ export default function Index() {
   }
 
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "position" : "height"}
+      keyboardVerticalOffset={-100}
+    >
       {/* Welcome Text */}
       <Text style={styles.header1}>Welcome to the App!</Text>
       <Text style={styles.text}>
@@ -47,20 +64,17 @@ export default function Index() {
         Pardon the emptiness, we&apos;re still building stuff out!
       </Text>
       {/* Login Form */}
-      <KeyboardAvoidingView
-        style={styles.formBox}
-        behavior={Platform.OS === "ios" ? "position" : "height"}
-        keyboardVerticalOffset={50}
-      >
+      <View style={styles.formBox}>
         {/* Username Input */}
         <TextInput
           placeholder="Username"
           value={username}
           onChangeText={setUsername}
           style={styles.textInput}
-          clearTextOnFocus={true}
           returnKeyType="next"
+          clearButtonMode={"while-editing"}
           onSubmitEditing={() => Pass.current?.focus()}
+          enterKeyHint="next"
         />
         {/* Password Input */}
         <TextInput
@@ -70,8 +84,9 @@ export default function Index() {
           onChangeText={setPassword}
           secureTextEntry={true}
           style={styles.textInput}
-          clearTextOnFocus={true}
+          clearButtonMode={"while-editing"}
           onSubmitEditing={handleLogin}
+          enterKeyHint="done"
         />
         {/* Form Submit Button */}
         <SimpleButton
@@ -86,8 +101,8 @@ export default function Index() {
             disabled ? styles.loginButtonDisabled : styles.loginButtonText
           }
         />
-      </KeyboardAvoidingView>
-    </View>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -95,34 +110,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
-    backgroundColor: "#666464",
+    backgroundColor: "#ffffff",
     alignItems: "center",
-    justifyContent: "flex-start",
-    paddingTop: 50,
+    justifyContent: "center",
   },
   text: {
-    color: "#fff",
+    color: "#000000",
     fontSize: 18,
     textAlign: "center",
     fontFamily: "Times New Roman",
     margin: 20,
   },
   header1: {
-    color: "Grey",
+    color: "white",
     fontSize: 22,
     textAlign: "center",
     fontFamily: "Arial",
     fontWeight: "bold",
+    backgroundSize: "contain",
     backgroundColor: "#9854ba",
-    padding: 10,
+    padding: 5,
     borderRadius: 15,
-    marginTop: 20,
+    margin: 10,
   },
   formBox: {
     margin: 20,
-    width: "60%",
     padding: 10,
-    backgroundColor: "#3c3a3d",
+    backgroundColor: "#040404",
     borderRadius: 10,
     justifyContent: "center",
   },
@@ -135,7 +149,7 @@ const styles = StyleSheet.create({
     color: "#fff",
   },
   loginButtonContainer: {
-    borderRadius: 5,
+    borderRadius: 10,
     padding: 10,
     backgroundColor: "#037dff",
   },
